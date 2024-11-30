df4158b6306ef0fbff253bfbc45dc3826ceb36ca
Paulo Cardoso <cardosopaulohenrique91@gmail.com>
2024-11-30 20:40:22 -0300

    commit jornada de dados

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Jornada de dados')
+
+# Varáveis
+idade = 32
+nome = 'Paulo'
+
+print(idade)
+
+# Soma
+nova_idade = idade + 1 
+print(nova_idade)
+
+# Lista
+list= [1,2,3,4,5]
+
+# Métodos
+lista_invertida = list.reverse()
+
